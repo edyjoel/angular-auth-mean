1be3adc87d84d904e29509d6fe03b89a68151fda
Edy <edyxicon@gmail.com>
2023-03-24 10:05:43 -0600

    Add backend index

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./db/config');
+require('dotenv').config();
+
+const app = express();
+
+dbConnection();
+
+app.use(express.static('public'));
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
