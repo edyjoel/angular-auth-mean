440a5b60663fc2ce1dfc3d4ab015935047c5ea96
Edy <edyxicon@gmail.com>
2023-03-26 23:31:11 -0600

    Add app angular to public

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./db/config');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -14,6 +15,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
